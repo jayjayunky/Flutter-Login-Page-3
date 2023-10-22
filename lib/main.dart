606f33ec5e097456e0e7432ac09a0116eca76872
Mohammad Rahmani <rto1680@gmail.com>
2023-10-22 13:53:34 +0330

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day14/Animation/FadeAnimation.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -18,9 +18,9 @@ class HomePage extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             colors: [
-              Colors.orange[900],
-              Colors.orange[800],
-              Colors.orange[400]
+              Colors.orange.shade900,
+              Colors.orange.shade800,
+              Colors.orange.shade400
             ]
           )
         ),
@@ -33,9 +33,9 @@ class HomePage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeAnimation(1, Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),)),
+                  FadeInUp(duration: Duration(milliseconds: 1000), child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),)),
                   SizedBox(height: 10,),
-                  FadeAnimation(1.3, Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
+                  FadeInUp(duration: Duration(milliseconds: 1300), child: Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
                 ],
               ),
             ),
@@ -46,13 +46,12 @@ class HomePage extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
                 ),
-                child: SingleChildScrollView(
-                  child: Padding(
+                child: Padding(
                     padding: EdgeInsets.all(30),
                     child: Column(
                       children: <Widget>[
                         SizedBox(height: 60,),
-                        FadeAnimation(1.4, Container(
+                        FadeInUp(duration: Duration(milliseconds: 1400), child: Container(
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10),
@@ -67,7 +66,7 @@ class HomePage extends StatelessWidget {
                               Container(
                                 padding: EdgeInsets.all(10),
                                 decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                                  border: Border(bottom: BorderSide(color: Colors.grey.shade200))
                                 ),
                                 child: TextField(
                                   decoration: InputDecoration(
@@ -80,9 +79,10 @@ class HomePage extends StatelessWidget {
                               Container(
                                 padding: EdgeInsets.all(10),
                                 decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                                  border: Border(bottom: BorderSide(color: Colors.grey.shade200))
                                 ),
                                 child: TextField(
+                                  obscureText: true,
                                   decoration: InputDecoration(
                                     hintText: "Password",
                                     hintStyle: TextStyle(color: Colors.grey),
@@ -94,30 +94,35 @@ class HomePage extends StatelessWidget {
                           ),
                         )),
                         SizedBox(height: 40,),
-                        FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
+                        FadeInUp(duration: Duration(milliseconds: 1500), child: Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
                         SizedBox(height: 40,),
-                        FadeAnimation(1.6, Container(
+                        FadeInUp(duration: Duration(milliseconds: 1600), child: MaterialButton(
+                          onPressed: () {},
                           height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
+                          // margin: EdgeInsets.symmetric(horizontal: 50),
+                          color: Colors.orange[900],
+                          shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(50),
-                            color: Colors.orange[900]
+
                           ),
+                          // decoration: BoxDecoration(
+                          // ),
                           child: Center(
                             child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
                           ),
                         )),
                         SizedBox(height: 50,),
-                        FadeAnimation(1.7, Text("Continue with social media", style: TextStyle(color: Colors.grey),)),
+                        FadeInUp(duration: Duration(milliseconds: 1700), child: Text("Continue with social media", style: TextStyle(color: Colors.grey),)),
                         SizedBox(height: 30,),
                         Row(
                           children: <Widget>[
                             Expanded(
-                              child: FadeAnimation(1.8, Container(
+                              child: FadeInUp(duration: Duration(milliseconds: 1800), child: MaterialButton(
+                                onPressed: (){},
                                 height: 50,
-                                decoration: BoxDecoration(
+                                color: Colors.blue,
+                                shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue
                                 ),
                                 child: Center(
                                   child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
@@ -126,12 +131,14 @@ class HomePage extends StatelessWidget {
                             ),
                             SizedBox(width: 30,),
                             Expanded(
-                              child: FadeAnimation(1.9, Container(
+                              child: FadeInUp(duration: Duration(milliseconds: 1900), child: MaterialButton(
+                                onPressed: () {},
                                 height: 50,
-                                decoration: BoxDecoration(
+                                shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black
+
                                 ),
+                                color: Colors.black,
                                 child: Center(
                                   child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
                                 ),
@@ -142,7 +149,6 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                ),
               ),
             )
           ],
